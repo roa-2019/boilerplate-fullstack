14d639bc99bbafcdceb9990f3da447def3abe5b5
Student <student@pop-os.localdomain>
2019-09-05 11:13:06 +1200

    boilerplate setup

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const express = require('express')
 
-const fruitRoutes = require('./routes/fruits')
+const routes = require('./routes/routes')
 
 const server = express()
 
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
+server.use('/api/v1/fruits', routes)
 
 module.exports = server
